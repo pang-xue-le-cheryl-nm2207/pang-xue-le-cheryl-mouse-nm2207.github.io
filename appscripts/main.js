12977a0a776a4df7980412800a0aa636c2d2df72
pang-xue-le-cheryl-nm2207 <125462621+pang-xue-le-cheryl-nm2207@users.noreply.github.com>
2023-02-15 20:08:27 +0800

    Add files via upload

---
+++ appscripts/main.js
@@ -0,0 +1,10 @@
+// main.js
+
+let visitorName = prompt("Hi visitor, what's your name?");
+if(visitorName != ""){
+    document.getElementById("visiting").innerHTML = "Hello, " + visitorName + " :) Welcome to my first ever website!";
+}
+
+
+
+console.log('yo yo yo !!! this is from main.js');
